4268854ea02f11f1911e373da0ad94bdd10d8638
FeyzaDc <feyzadecdeli@hotmail.com>
2022-04-24 00:08:50 +0300

    classes added

---
+++ Meowy_/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Meowy_.Models
+{
+    public class User
+    {
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }        
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public DateTime birthdate { get; set; }
+        public DateTime creation_date { get; set; }
+        
+        /* image eklenecek */
+
+        public bool is_priv { get; set; }
+
+    }
+}
