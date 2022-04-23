ba58afa8d86af93922c69cd776e940c9a87d4896
FeyzaDc <feyzadecdeli@hotmail.com>
2022-04-23 23:04:17 +0300

    pages added

---
+++ Meowy_/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Meowy_.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Meowy_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Explore()
+        {
+            return View();
+        }
+
+        public IActionResult Notifications()
+        {
+            return View();
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+        public IActionResult Bookmarks()
+        {
+            return View();
+        }
+
+        public IActionResult FRequests()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
